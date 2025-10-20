d8c6c95d1c9841d793a91f34f57c78d1d79d8025
HarryDigo <rodrigo.scatolin.rio@gmail.com>
2025-10-19 21:16:47 -0300

    changed user post return value

--- routes/user.js
+++ routes/user.js
@@ -51,7 +51,7 @@ users.post('/', async (req, res) => {
     }
 
     const result = await collection.insertOne(newUser)
-    res.send(result).status(201)
+    res.send(newUser).status(201)
   } catch {
     res.status(500).send('Error creating user')
   }
